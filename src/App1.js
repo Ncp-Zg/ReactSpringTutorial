12d9edf5cfc409ad1048b40a30d961654e6cd5a6
Necip <ncptzgn@gmail.com>
2021-10-04 18:01:08 +0300

    React spring tutorial -random number generator with animation

---
+++ src/App1.js
@@ -0,0 +1,32 @@
+import "./styles.css"
+import React, { useState } from 'react'
+import {animated,useSpring} from "react-spring"
+
+const App1 = () => {
+
+    const [numb,setNumb] =useState(999)
+
+    //use spring hook is used to animate the value
+
+   
+    
+    //will naimate from 0 to 999 
+    const props = useSpring({val:numb , from: {val:0}})
+
+    //generate random number
+    
+    const randomNumber = Math.floor(Math.random()*999)
+console.log(props.val)
+    //get rid of decimals
+    return (
+        <div className="App">
+            <div className="card">
+                <animated.h1>{props.val.to(val=>Math.floor(val))}</animated.h1>
+
+                <button className="btn" onClick={()=>setNumb(randomNumber)}>Generate Number</button>
+            </div>
+        </div>
+    )
+}
+
+export default App1
