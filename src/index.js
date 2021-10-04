982790df9e9a37cee37fa18d9e72b9c611ec9e62
Necip <ncptzgn@gmail.com>
2021-10-04 19:13:32 +0300

    React spring tutorial -using useTransition

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import App1 from './App1';
+import App2 from './App2';
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <App1 />
+    <App2 />
   </React.StrictMode>,
   document.getElementById('root')
 );
